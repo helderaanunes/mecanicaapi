5d9a9bac866924b0214bc165568a194972bced83
helderaanunes <158058420+AndersonCamposs@users.noreply.github.com>
2026-03-03 20:02:00 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpe/mecanica/MecanicaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.mecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecanicaApplication.class, args);
+	}
+
+}
