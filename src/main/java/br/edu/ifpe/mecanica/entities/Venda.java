fccf4169b83a4e8fedb1ee04f4a1d77a6e70cdd3
brunovsant01 <brunovsant04@gmail.com>
2026-03-10 19:33:11 -0300

    Create Venda.java

---
+++ src/main/java/br/edu/ifpe/mecanica/entities/Venda.java
@@ -0,0 +1,24 @@
+package br.edu.ifpe.mecanica.entities;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime dataHora;
+    private double valorTotal;
+    private double desconto;
+    private double valorLiquido;
+    private Usuario usuario;
+
+}
