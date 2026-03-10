67b38fd320ea924b8c6f5ddc3ea77072c7162fce
Your Name <josejjonhson@gmail.com>
2026-03-10 19:53:33 -0300

    Entidade e Repository de Veciculo e AgendamentoServico

---
+++ src/main/java/br/edu/ifpe/mecanica/repositories/VeiculoRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifpe.mecanica.repositories;
+
+
+import br.edu.ifpe.mecanica.entities.Veiculo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends CrudRepository<Veiculo, Long> {
+}
